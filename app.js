3abb4f7b5ad973d0f74887a9bfebdfccd7f4a4dc
Sam-Dunne <cras.sdunne@gmail.com>
2021-01-18 22:11:18 -0600

    Finished

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+let friends = ['Chase', 'Trey', 'Katie', 'Devonte', 'Emil'];
+
+for (let name = 0; name < friends.length; name++) {
+    console.log(friends[name]);
+
+    for (let num = 100; num >= 1; num--) {
+        if (num === 2) {
+            console.log(num + ' lines of code in the file, '
+                + num + ' lines of code; ' + friends[name] + ' strikes one out, clears it all out, '
+                + (num - 1) + ' line of code in the file,')
+        } else if (num === 1) {
+            console.log(num + ' line of code in the file, '
+                + num + ' line of code; ' + friends[name] + ' strikes one out, clears it all out, '
+                + (num - 1) + ' lines of code in the file.')
+        } else {
+            console.log(num + ' lines of code in the file, '
+                + num + ' lines of code; ' + friends[name] + ' strikes one out, clears it all out '
+                + (num - 1) + ' lines of code in the file,')
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+

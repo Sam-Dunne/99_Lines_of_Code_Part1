18a34477e943a46980576dcf054c5283f6fdd40a
Sam-Dunne <cras.sdunne@gmail.com>
2021-01-25 22:12:07 -0600

    cleanup-update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let friends = ['Chase', 'Trey', 'Katie', 'Devonte', 'Emil'];
 for (let name = 0; name < friends.length; name++) {
     console.log(friends[name]);
 
-    for (let num = 100; num >= 1; num--) {
+    for (let num = 99; num >= 1; num--) {
         if (num === 2) {
             console.log(num + ' lines of code in the file, '
                 + num + ' lines of code; ' + friends[name] + ' strikes one out, clears it all out, '
@@ -18,11 +18,8 @@ for (let name = 0; name < friends.length; name++) {
             console.log(num + ' lines of code in the file, '
                 + num + ' lines of code; ' + friends[name] + ' strikes one out, clears it all out '
                 + (num - 1) + ' lines of code in the file,')
-
         }
-
     }
-
 }
 
 
